5fd07b11f32cd8875a89babea8316cd9fe5a76ea
FiveXSeven <nazairesalomon333@gmail.com>
2025-01-05 14:07:10 +0000

    setting springboot security customize

--- src/main/java/com/nazaire/product_crud/config/SecurityConfig.java
+++ src/main/java/com/nazaire/product_crud/config/SecurityConfig.java
@@ -1,4 +1,27 @@
 package com.nazaire.product_crud.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                    .csrf(AbstractHttpConfigurer::disable)
+                    .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                    .formLogin(Customizer.withDefaults())
+                    .httpBasic(Customizer.withDefaults())
+
+                    .build();
+    }
+
 }
